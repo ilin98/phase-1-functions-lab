fb8755d6aa5b1baa147498608a05a52d4541301c
Isaac <isaac.lin.pnc@gmail.com>
2023-01-09 12:32:36 -0500

    Finished lab.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(num) {
+    if (num > 42) {
+        return num - 42;
+    } else if (num < 42) {
+        return 42 - num;
+    } else {
+        return 0;
+    }
+};
+
+function distanceFromHqInFeet(num) {
+    return distanceFromHqInBlocks(num) *  264;
+};
+
+function distanceTravelledInFeet(start, destination) {
+    if (start < destination) {
+        return (destination - start) * 264;
+    } else if (destination < start) {
+        return (start - destination) * 264;
+    } else {
+        return 0;
+    }
+};
+
+function calculatesFarePrice(start, destination) {
+    let feet = distanceTravelledInFeet(start, destination);
+    if (feet <= 400) {
+        return 0;
+    } else if (feet <= 2000) {
+        return (feet - 400) * 0.02;
+    } else if (feet <= 2500) {
+        return 25;
+    } else {
+        return "cannot travel that far";
+    }
+}
